32d3e6c6842fab57348437fd8df0bcf2333855e4
rabiulahsan <ra.rabiulahsan@gmail.com>
2023-05-21 01:48:55 +0600

    bookmark added

--- src/Shared/SingleCard.jsx
+++ src/Shared/SingleCard.jsx
@@ -1,21 +1,28 @@
 /* eslint-disable react/prop-types */
 // import React from 'react';
-import { AiOutlineHeart, AiOutlineArrowRight } from "react-icons/ai";
+import { AiFillHeart, AiOutlineArrowRight } from "react-icons/ai";
 import { Rating } from "@smastrom/react-rating";
 
 import "@smastrom/react-rating/style.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const SingleCard = ({ product }) => {
   const { _id, title, photoURL, ratings, price, description } = product;
-
+  const [love, setLove] = useState(false);
+  const handleBookmark = () => {
+    setLove(!love);
+  };
   return (
     <div className="card-hover p-4 rounded-lg border border-white">
       <img className="h-[200px] mx-auto rounded-lg" src={photoURL} alt="" />
       <div className="flex justify-between items-center my-2">
         <p className="font-semibold text-xl">{title}</p>
-        <span className="text-xl">
-          <AiOutlineHeart></AiOutlineHeart>
+        <span
+          className={`text-xl cursor-pointer ${love ? "text-red-600" : ""}`}
+          onClick={handleBookmark}
+        >
+          <AiFillHeart></AiFillHeart>
         </span>
       </div>
       <p>
