420bbad3eaf74b62f01950158b795eb75c71286f
rabiulahsan <ra.rabiulahsan@gmail.com>
2023-05-21 03:01:15 +0600

    footer section added

---
+++ src/Pages/Home/Footer.jsx
@@ -0,0 +1,41 @@
+// import React from 'react';
+import { AiFillFacebook, AiFillInstagram } from "react-icons/ai";
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+        <div className="grid grid-flow-col gap-4">
+          <Link to="/" className="link link-hover">
+            About us
+          </Link>
+          <Link to="/" className="link link-hover">
+            Contact
+          </Link>
+          <Link to="alltoys" className="link link-hover">
+            All Toys
+          </Link>
+        </div>
+        <div>
+          <div className="grid grid-flow-col gap-4">
+            <span className="text-2xl">
+              <Link to="">
+                <AiFillFacebook></AiFillFacebook>
+              </Link>
+            </span>
+            <span className="text-2xl">
+              <Link to="">
+                <AiFillInstagram></AiFillInstagram>
+              </Link>
+            </span>
+          </div>
+        </div>
+        <div>
+          <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@
 
 import Banner from "./Banner";
 import Discount from "./Discount";
+import Footer from "./Footer";
 import Trending from "./Trending";
 
 const Home = () => {
@@ -13,6 +14,8 @@ const Home = () => {
       <Discount></Discount>
       {/* trending section  */}
       <Trending></Trending>
+      {/* footer section  */}
+      <Footer></Footer>
     </div>
   );
 };
