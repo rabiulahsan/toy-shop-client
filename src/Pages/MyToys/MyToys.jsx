3b8e362b157aa765faaf94cfdb5dedfbf4cdcf05
rabiulahsan <ra.rabiulahsan@gmail.com>
2023-05-23 15:14:03 +0600

    mytoys design completed

--- src/Pages/MyToys/MyToys.jsx
+++ src/Pages/MyToys/MyToys.jsx
@@ -6,23 +6,66 @@ import MyToysRow from "./MyToysRow";
 const MyToys = () => {
   const { user } = useContext(AuthContext);
   const [myToys, setMyToys] = useState([]);
-  console.log(user);
-  // fetch(`http://localhost:5000/mytoys?email=${user?.email}`)
+
   useEffect(() => {
+    // fetch(`http://localhost:5000/mytoys?email=${user?.email}`)
     fetch("http://localhost:5000/")
       .then((res) => res.json())
       .then((data) => setMyToys(data));
   }, []);
 
+  //update function
+  const handleUpdate = (id) => {
+    // fetch(`http://localhost:5000/bookings/${id}`, {
+    //   method: "PATCH",
+    //   headers: {
+    //     "content-type": "application/json",
+    //   },
+    //   body: JSON.stringify({ status: "confirm" }),
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     if (data.modifiedCount > 0) {
+    //       // update state
+    //       const remaining = bookings.filter((booking) => booking._id !== id);
+    //       const updated = bookings.find((booking) => booking._id === id);
+    //       updated.status = "confirm";
+    //       const newBookings = [updated, ...remaining];
+    //       setBookings(newBookings);
+    //     }
+    //   });
+  };
+
+  //delete function
+  const handleDelete = (id) => {
+    const proceed = confirm("Are You sure you want to delete");
+    if (proceed) {
+      fetch(`http://localhost:5000/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("deleted successful");
+            const remaining = myToys.filter((booking) => booking._id !== id);
+            setMyToys(remaining);
+          }
+        });
+    }
+  };
+
   console.log(myToys);
   return (
-    <div>
-      <table className="table-fixed w-full ">
+    <div className="my-[5%] px-[8%]">
+      <table className="table-fixed w-full bg-transparent">
         {/* head */}
-        <thead className="border border-t-0 border-l-0 border-r-0 border-b-gray-500 bg-gray-100">
+        <thead className="border border-t-0 border-l-0 border-r-0 border-b-gray-500 bg-transparent">
           <tr>
             <th className="py-5">Image</th>
             <th>Name</th>
+            <th>Price</th>
             <th>Seller Email</th>
             <th>Update</th>
             <th>Delete</th>
@@ -30,7 +73,12 @@ const MyToys = () => {
         </thead>
         <tbody>
           {myToys.map((toy) => (
-            <MyToysRow key={toy._id} toy={toy}></MyToysRow>
+            <MyToysRow
+              key={toy._id}
+              toy={toy}
+              handleDelete={handleDelete}
+              handleUpdate={handleUpdate}
+            ></MyToysRow>
           ))}
         </tbody>
       </table>
--- src/Pages/MyToys/MyToysRow.jsx
+++ src/Pages/MyToys/MyToysRow.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+// /* eslint-disable react/prop-types */
+// // import React from "react";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 
-const MyToysRow = () => {
-  return <div></div>;
+const MyToysRow = ({ toy, handleDelete, handleUpdate }) => {
+  console.log(toy._id);
+
+  const { _id, name, email, photoURL, price } = toy;
+  return (
+    <tr className="hover:bg-gray-100 hover:text-black text-white  w-full text-center  ">
+      <td className="py-5 ">
+        <img src={photoURL} alt="" className=" h-16 rounded-2xl mx-auto" />
+      </td>
+      <td>{name}</td>
+      <td>{price}</td>
+      <td className="">{email}</td>
+      <td>
+        <span onClick={() => handleUpdate(_id)}>
+          <AiFillEdit></AiFillEdit>
+        </span>
+      </td>
+      <td>
+        <span onClick={() => handleDelete(_id)}>
+          <AiFillDelete></AiFillDelete>
+        </span>
+      </td>
+    </tr>
+  );
 };
 
 export default MyToysRow;
