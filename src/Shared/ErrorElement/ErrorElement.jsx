2238a94467a80d9297347e27de7c703c84b53542
rabiulahsan <ra.rabiulahsan@gmail.com>
2023-06-16 22:02:49 +0600

    error page fixed

--- src/Shared/ErrorElement/ErrorElement.jsx
+++ src/Shared/ErrorElement/ErrorElement.jsx
@@ -6,27 +6,12 @@ const ErrorElement = () => {
   //   console.log(status);
   console.log(error);
   return (
-    <div>
-      <div className="w-28 h-[710px] d-flex justify-content-center align-items-center  text-center">
-        <p className="">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="mx-auto"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
-            />
-          </svg>
-        </p>
-        <p className=" text-4xl fw-bold">404</p>
-        <p className="text-2xl ">Route Not Found</p>
-      </div>
+    <div className="w-full h-[710px]">
+      <img
+        className="h-full w-full"
+        src="https://ipx-cdn.lottiefiles.com/X8T2hwy9tg623aiXImlKnvixcScV-N66PGhmJsqUeQ8/fill/1200/600/no/0/aHR0cHM6Ly9kM2psNzY5b3k2OXk3Yi5jbG91ZGZyb250Lm5ldC8yMDIyLzA4LzQwNC1QYWdlLUJsb2ctQ292ZXIucG5n.jpg"
+        alt=""
+      />
     </div>
   );
 };
